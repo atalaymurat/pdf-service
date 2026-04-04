a851ac1cfc4473a1c6872dc2822a0d0c96cfb298
atalaymurat <atalaymurat@msn.com>
2026-04-04 15:58:50 +0300

    debug: include error detail in 500 response

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -6,6 +6,6 @@ exports.generatePdf = async (req, res) => {
     await generateQuotationPdf(req.body, res);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Failed to generate PDF" });
+    res.status(500).json({ error: "Failed to generate PDF", detail: err.message, stack: err.stack });
   }
 };
